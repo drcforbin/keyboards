0070bf6f83f29fc874dd40e6d812f60d8f30687d
drcforbin <ryan@c.oloss.us>
2020-07-01 11:14:50 -0500

    added crkbd to soft link script

--- link.sh
+++ link.sh
@@ -5,3 +5,9 @@ mkdir -p qmk/keyboards/planck/keymaps/rcw
 ln -sf $(pwd)/planck/config.h qmk/keyboards/planck/keymaps/rcw/
 ln -sf $(pwd)/planck/keymap.c qmk/keyboards/planck/keymaps/rcw/
 ln -sf $(pwd)/planck/rules.mk qmk/keyboards/planck/keymaps/rcw/
+
+mkdir -p qmk/keyboards/crkbd/keymaps/rcw
+
+ln -sf $(pwd)/crkbd/config.h qmk/keyboards/crkbd/keymaps/rcw/
+ln -sf $(pwd)/crkbd/keymap.c qmk/keyboards/crkbd/keymaps/rcw/
+ln -sf $(pwd)/crkbd/rules.mk qmk/keyboards/crkbd/keymaps/rcw/
